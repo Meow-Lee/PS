eb54cc1d46a90e266d5feeceeea75865700d52ec
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2022-08-03 17:02:42 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int T, a, b;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin>>T;
+    while(T--){
+        cin>>a>>b;
+        cout<<a+b<<'\n';
+    }
+}
