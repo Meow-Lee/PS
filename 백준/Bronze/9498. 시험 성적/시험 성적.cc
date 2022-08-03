0a73740348d5bedf246018f5791639da1589df5c
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2022-08-03 17:07:28 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin>>n;
+
+    if(n>=90 && n<=100){
+        cout<<'A';
+    }
+    else if(n>=80){
+        cout<<'B';
+    }
+    else if(n>=70){
+        cout<<'C';
+    }
+    else if(n>=60){
+        cout<<'D';
+    }
+    else{
+        cout<<'F';
+    }
+}
