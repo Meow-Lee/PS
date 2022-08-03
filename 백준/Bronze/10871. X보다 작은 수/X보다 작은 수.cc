dfee669146f835b9dba972e54e621246b029e478
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2022-08-03 16:59:38 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, x, k;
+    cin>>n>>x;
+
+    for(int i=0; i<n; i++){
+        cin>>k;
+        if(k<x){
+            cout<<k<<" ";
+        }
+    }
+}
