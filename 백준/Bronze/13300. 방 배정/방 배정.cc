2142b34fa117ffcc7f7d19908d167d7f1991ed94
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2022-08-04 16:02:52 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, k;
+int arr[2][7];
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin>>n>>k;
+
+    for(int i=0; i<n; i++){
+        int s, y;
+        cin>>s>>y;
+
+        arr[s][y]++;
+    }
+
+    int cnt=0;
+
+    for(int i=0; i<2; i++){
+        for(int j=1; j<7; j++){
+            cnt += arr[i][j]/k;
+            if(arr[i][j] % k){
+                cnt++;
+            }
+        }
+    }
+    cout<<cnt;
+}
