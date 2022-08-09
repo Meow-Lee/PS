017be6ae14f872b3ae7f8800387e9fcd34a48933
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2022-08-09 14:58:17 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin>>n;
+
+    queue<int> Q;
+    for(int i=1; i<=n; i++){
+        Q.push(i);
+    }
+
+    while(Q.size()!=1){
+        Q.pop();
+
+        auto t = Q.front();
+        Q.pop();
+        Q.push(t);
+    }
+    cout<<Q.front();
+}
