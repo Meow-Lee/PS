3620b679b5f11bd098f908263e2ccf6add654b1b
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2022-08-12 16:38:27 +0900

    [Gold IV] Title: N-Queen, Time: 1696 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int N, cnt=0;
+bool isused1[40], isused2[40], isused3[40];
+
+void func(int cur){
+    if(cur==N){
+        cnt++;
+        return;
+    }
+
+    for(int i=0; i<N; i++){
+        if(isused1[i] || isused2[cur+i] || isused3[cur-i+N-1]){
+            continue;
+        }
+
+        isused1[i]=1;
+        isused2[cur+i]=1;
+        isused3[cur-i+N-1]=1;
+        
+        func(cur+1);
+
+        isused1[i]=0;
+        isused2[cur+i]=0;
+        isused3[cur-i+N-1]=0;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    cin>>N;
+    func(0);
+    cout<<cnt;
+}
