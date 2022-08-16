8b2f82db7da9333d98894dc4b285d8d859064a77
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2022-08-16 13:18:15 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int A,B;
+
+    while(true){
+        cin>>A>>B;
+        if(cin.eof()){
+            break;
+        }
+
+        cout<<A+B<<'\n';
+    }
+}
