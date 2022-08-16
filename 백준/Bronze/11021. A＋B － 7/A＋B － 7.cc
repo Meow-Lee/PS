96c09e7cc9987066bf098e463cb08c452e3c4c7d
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2022-08-16 13:24:28 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int T,A,B;
+
+    cin>>T;
+    int cnt=1;
+    while(T--){
+        cin>>A>>B;
+        cout<<"Case #"<<cnt++<<": "<<A+B<<'\n';
+    }
+}
