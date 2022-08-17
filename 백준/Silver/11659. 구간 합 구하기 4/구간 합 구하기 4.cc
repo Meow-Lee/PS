baf91d8d894f76b5340bf032840ceb716b046647
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2022-08-17 16:38:53 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int d[100001];
+int a[100001];
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int n,m;
+    cin>>n>>m;
+
+    d[0]=0;
+    for(int i=1; i<=n; i++){
+        cin>>a[i];
+        d[i] = d[i-1]+a[i];
+    }
+
+    while(m--){
+        int i,j;
+        cin>>i>>j;
+
+        cout<<d[j] - d[i-1]<<'\n';
+    }
+}
