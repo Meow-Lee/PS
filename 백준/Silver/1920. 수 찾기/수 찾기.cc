f8fa16fdf043563df7f45d33783f0f5870f13eaf
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2022-08-20 13:55:55 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+int arr[100005];
+
+int func(int target){
+    int st = 0;
+    int en = n-1;
+
+    while(st<=en){
+        int mid = (st+en)/2;
+
+        if(arr[mid]<target){
+            st = mid + 1;
+        }
+        else if(arr[mid]>target){
+            en = mid - 1;
+        }
+        else{
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    sort(arr, arr+n);
+
+    int m;
+    cin>>m;
+
+    while(m--){
+        int t;
+        cin>>t;
+
+        cout<<func(t)<<'\n';
+    }
+}
