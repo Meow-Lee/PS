89803fab9419947ed384eddcac9f7131aea6bb1d
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2023-02-14 15:40:45 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    
+    for(int i=0; i<my_string.length(); i++){
+        switch(my_string[i]){
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                continue;
+            default:
+                answer += my_string[i];
+        }
+    }
+    return answer;
+}
