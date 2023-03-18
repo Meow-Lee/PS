7bfd00d11c77255912fa0d92683976b14c7164d0
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2023-03-18 10:39:08 +0900

    [Unrated] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int d[1001];
+    d[1] = 1;
+    d[2] = 3;
+    for(int i=3; i<=n; i++){
+        d[i] = (d[i - 1] + 2 * d[i - 2]) % 10007;
+    }
+    cout<<d[n];
+}
