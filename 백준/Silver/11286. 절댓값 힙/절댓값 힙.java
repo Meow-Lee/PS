cc4f97af09a9595c4d1afaa04c3dd252dea8c110
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2023-06-15 18:32:28 +0900

    [Silver I] Title: 절댓값 힙, Time: 320 ms, Memory: 25408 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -2,9 +2,16 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) ->
-        Math.abs(o1) == Math.abs(o2) ? Integer.compare(o1, o2) : Integer.compare(Math.abs(o1), Math.abs(o2))
-    );
+    static PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+        int a = Math.abs(o1);
+        int b = Math.abs(o2);
+
+        if (a == b) {
+            return o1 > o2 ? 1 : -1;
+        } else {
+            return a - b;
+        }
+    });
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
