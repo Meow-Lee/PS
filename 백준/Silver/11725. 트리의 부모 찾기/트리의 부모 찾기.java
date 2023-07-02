23a7269bb7696ba85fe32d3427de6f2a379b88db
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2023-07-02 13:40:18 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1432 ms, Memory: 74400 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,46 +1,55 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    static boolean[] visited;
+public class Main {
+    static int n;
+    static ArrayList<Integer>[] list;
+    static boolean[] visit;
     static int[] parent;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
-        
-        int n = Integer.parseInt(stk.nextToken());
 
-        ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
-        for(int i=1; i<=n+1; i++){
-            tree.add(new ArrayList<>());
+        n = Integer.parseInt(stk.nextToken());
+
+        list = new ArrayList[n + 1];
+        visit = new boolean[n + 1];
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<>();
         }
 
-        for(int i=1; i<n; i++){
+        for (int i = 0; i < n - 1; i++) {
             stk = new StringTokenizer(br.readLine(), " ");
-            int x = Integer.parseInt(stk.nextToken());
-            int y = Integer.parseInt(stk.nextToken());
-            tree.get(x).add(y);
-            tree.get(y).add(x);
-        }
-        visited = new boolean[n+1];
-        parent = new int[n+1];
 
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(1);
-        visited[1] = true;
-        while(!q.isEmpty()){
-            int v = q.poll();
-            for(int node : tree.get(v)){
-                if(!visited[node]){
-                    visited[node]=true;
-                    q.add(node);
-                    parent[node] = v;
-                }
-            }
-        }
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
 
-        for(int i=2; i<=n; i++){
+            list[a].add(b);
+            list[b].add(a);
+        }
+        bfs(1);
+
+        for (int i = 2; i <= n; i++) {
             System.out.println(parent[i]);
+        }
+    }
+
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visit[start] = true;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            for (int node : list[cur]) {
+                if (visit[node]) {
+                    continue;
+                }
+                q.offer(node);
+                parent[node] = cur;
+                visit[node] = true;
+            }
         }
     }
 }
