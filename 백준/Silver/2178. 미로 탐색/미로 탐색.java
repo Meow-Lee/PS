48ca5e8a8912a42eb254e8f15e35754a3ed46782
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2023-07-03 14:02:39 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,53 +1,56 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    static int[][] board;
+public class Main {
+    static int n, m;
+    static String[] map;
     static int[][] dist;
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
-        
-        int n = Integer.parseInt(stk.nextToken());
-        int m = Integer.parseInt(stk.nextToken());
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    static class Pair{
+        int x, y;
 
-        board = new int[n][m];
-        dist = new int[n][m];
-        for(int i=0; i<n; i++){
-            String s = br.readLine();
-            for(int j=0; j<m; j++){
-                board[i][j] = s.charAt(j)-'0';
-            }
-        }
-
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(0,0));
-        dist[0][0] = 1;
-        while(!q.isEmpty()){
-            Node cur = q.poll();
-            for(int dir=0; dir<4; dir++){
-                int nx = cur.x + dx[dir];
-                int ny = cur.y + dy[dir];
-                if(nx<0 || ny<0 || nx>=n || ny>=m){
-                    continue;
-                }
-                if(board[nx][ny]==0 || dist[nx][ny]>0){
-                    continue;
-                }
-                q.offer(new Node(nx,ny));
-                dist[nx][ny] = dist[cur.x][cur.y]+1;
-            }
-        }
-        System.out.println(dist[n-1][m-1]);
-    }
-
-    public static class Node{
-        int x,y;
-        public Node(int x, int y){
+        Pair(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+
+        map = new String[n];
+        dist = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            map[i] = br.readLine();
+        }
+        bfs(0, 0);
+
+        System.out.println(dist[n - 1][m - 1]);
+    }
+
+    static void bfs(int x, int y) {
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(x, y));
+        dist[x][y] = 1;
+
+        while (!q.isEmpty()) {
+            Pair cur = q.poll();
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = cur.x + dx[dir];
+                int ny = cur.y + dy[dir];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+                    continue;
+                }
+                if (map[nx].charAt(ny) == '0' || dist[nx][ny] > 0) {
+                    continue;
+                }
+                q.offer(new Pair(nx, ny));
+                dist[nx][ny] = dist[cur.x][cur.y] + 1;
+            }
         }
     }
 }
