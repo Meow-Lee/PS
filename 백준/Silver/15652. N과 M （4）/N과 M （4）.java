c22d7a6004dd4fadc7a699672a7213e36b4bf3c6
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2023-07-07 15:22:58 +0900

    [Silver III] Title: N과 M (4), Time: 156 ms, Memory: 18348 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    static int[] selected;
+    static StringBuilder sb;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+        sb = new StringBuilder();
+
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+
+        selected = new int[m + 1];
+        rec_func(1);
+
+        System.out.println(sb.toString());
+    }
+
+    static void rec_func(int k) {
+        if (k == m + 1) {
+            for (int i = 1; i <= m; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            int start = selected[k - 1];
+            if (start == 0) {
+                start = 1;
+            }
+            for (int i = start; i <= n; i++) {
+                selected[k] = i;
+                rec_func(k + 1);
+                selected[k] = 0;
+            }
+        }
+    }
+}
