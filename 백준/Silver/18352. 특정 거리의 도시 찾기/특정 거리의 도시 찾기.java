b73f0c91b354c6d1c2571b1f0e089531665f887c
Lee-MyungMo <67636286+Meow-Lee@users.noreply.github.com>
2023-07-29 11:47:23 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1304 ms, Memory: 284196 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,75 +1,75 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
-    static class Info{
-        int v, d;
-        Info(int v, int d) {
+public class Main {
+    static int N, M, K, X;
+    static int[] dist;
+    static ArrayList<Node>[] lists;
+
+    static class Node implements Comparable<Node> {
+        int v, cost;
+
+        Node(int v, int cost) {
             this.v = v;
-            this.d = d;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node node) {
+            return this.cost - node.cost;
         }
     }
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
 
-        int n = Integer.parseInt(stk.nextToken());
-        int m = Integer.parseInt(stk.nextToken());
-        int k = Integer.parseInt(stk.nextToken());
-        int x = Integer.parseInt(stk.nextToken());
+        N = Integer.parseInt(stk.nextToken());
+        M = Integer.parseInt(stk.nextToken());
+        K = Integer.parseInt(stk.nextToken());
+        X = Integer.parseInt(stk.nextToken());
 
-        List<List<Info>> graph = new ArrayList<>(n+1);
-        for(int i=0; i<=n; i++){
-            graph.add(new ArrayList<>());
+        lists = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            lists[i] = new ArrayList<>();
         }
+        dist = new int[N + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < M; i++) {
             stk = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(stk.nextToken());
-            int b = Integer.parseInt(stk.nextToken());
 
-            graph.get(a).add(new Info(b, 1));
+            int u = Integer.parseInt(stk.nextToken());
+            int v = Integer.parseInt(stk.nextToken());
+
+            lists[u].add(new Node(v, 1));
         }
 
-        int[] dist = dijkstra(graph, x);
-        boolean flag = false;
-        for (int i = 0; i < dist.length; i++) {
-            if (dist[i] == k) {
-                flag = true;
-                System.out.println(i);
-            }
-        }
-        if(!flag){
-            System.out.println(-1);
-        }
-    }
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(X, 0));
+        dist[X] = 0;
 
-    public static int[] dijkstra(List<List<Info>> graph, int start) {
-        int[] dist = new int[graph.size()+1];
-        for (int i = 0; i < dist.length; i++) {
-            if (i == start) {
-                dist[i] = 0;
-            }
-            else{
-                dist[i] = Integer.MAX_VALUE;
-            }
-        }
-
-        PriorityQueue<Info> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.d));
-        pq.offer(new Info(start, 0));
         while (!pq.isEmpty()) {
-            Info cur = pq.poll();
-            if (cur.d > dist[cur.v]) {
-                continue;
-            }
-            for (Info node : graph.get(cur.v)) {
-                if (dist[node.v] <= node.d + dist[cur.v]) {
+            Node cur = pq.poll();
+            for (Node next : lists[cur.v]) {
+                if (dist[next.v] != Integer.MAX_VALUE) {
                     continue;
                 }
-                dist[node.v] = node.d + dist[cur.v];
-                pq.offer(new Info(node.v, dist[node.v]));
+                if (dist[next.v] > dist[cur.v] + next.cost) {
+                    dist[next.v] = dist[cur.v] + next.cost;
+                    pq.offer(new Node(next.v, dist[next.v]));
+                }
             }
         }
-        return dist;
+
+        boolean hasCity = false;
+        for (int i = 1; i <= N; i++) {
+            if (dist[i] == K) {
+                System.out.println(i);
+                hasCity = true;
+            }
+        }
+        if (!hasCity) {
+            System.out.println(-1);
+        }
     }
 }
